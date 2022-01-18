fda6ed7051a625d1cf5d8750891a0a445e9486e3
megajon <jonathan.seubert@megajon.com>
2022-01-18 15:33:33 -0800

    changed graphql api back to public

--- apollo-client.js
+++ apollo-client.js
@@ -3,7 +3,7 @@ import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 
 
 const client = new ApolloClient({
-    link: new HttpLink({ uri: 'http://localhost:3001/api/graphql', fetch }),
+    link: new HttpLink({ uri: 'https://megajon-web-api.herokuapp.com/api/graphql', fetch }),
     // uri: "http://localhost:3001/api/graphql",
     cache: new InMemoryCache(),
 });
