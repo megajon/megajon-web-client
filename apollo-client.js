3bb155168ae308d0d89ff881077c2dcefd846f15
megajon <jonathan.seubert@megajon.com>
2022-02-07 14:11:59 -0800

    installed dotenv

--- apollo-client.js
+++ apollo-client.js
@@ -3,7 +3,8 @@ import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 
 
 const client = new ApolloClient({
-    link: new HttpLink({ uri: 'https://megajon-web-api.herokuapp.com/api/graphql', fetch }),
+    // link: new HttpLink({ uri: 'https://megajon-web-api.herokuapp.com/api/graphql', fetch }),
+    link: new HttpLink({ uri: 'http://localhost:3001/api/graphql', fetch }),
     // uri: "http://localhost:3001/api/graphql",
     cache: new InMemoryCache(),
 });
