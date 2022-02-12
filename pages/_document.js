98810100624664c4e3b4bd2907dd2fd154e0d632
megajon <jonathan.seubert@megajon.com>
2022-02-11 17:15:32 -0800

    Fixed typo in _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,7 @@ export default class MyDocument extends Document {
           <script async src="https://www.googletagmanager.com/gtag/js?id=G-96HW8XQYYP"></script>
           <script>
             window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
+            function gtag(){dataLayer.push(arguments)}
             gtag('js', new Date());
 
             gtag('config', 'G-96HW8XQYYP');
