fdf8a0a42ee87e93f3ee6842cd0a3609d5c18662
megajon <jonathan.seubert@megajon.com>
2022-02-11 17:07:54 -0800

    new gtag

--- pages/_document.js
+++ pages/_document.js
@@ -5,23 +5,14 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          {/* Global Site Tag (gtag.js) - Google Analytics */}
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-96HW8XQYYP"></script>
+          <script>
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-              page_path: window.location.pathname,
-            });
-          `,
-            }}
-          />
+
+            gtag('config', 'G-96HW8XQYYP');
+          </script>
         </Head>
         <body>
           <Main />
