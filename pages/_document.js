4fcb40b1e87e927f3008513704ac9464d9b27efd
megajon <jonathan.seubert@megajon.com>
2022-02-11 23:29:12 -0800

    removed extra backtick _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,7 @@ export default class MyDocument extends Document {
           <script>
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments)}
-            gtag(`js``, new Date());
+            gtag(`js`, new Date());
 
             gtag(`config`, `G-96HW8XQYYP`);
           </script>
