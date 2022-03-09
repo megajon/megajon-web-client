f47552141a3e5913fbc90ee0806851a57b12126c
megajon <jonathan.seubert@megajon.com>
2022-03-09 13:10:03 -0800

    added nextjs gtag script

--- pages/_document.js
+++ pages/_document.js
@@ -1,18 +1,25 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script';
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html>
         <Head>
-          <script async src="https://www.googletagmanager.com/gtag/js?id=G-96HW8XQYYP"></script>
-          <script>
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments)}
-            gtag(`js`, new Date());
+          {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+          <Script
+            src="https://www.googletagmanager.com/gtag/js?id=G-96HW8XQYYP"
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){window.dataLayer.push(arguments);}
+              gtag('js', new Date());
 
-            gtag(`config`, `G-96HW8XQYYP`);
-          </script>
+              gtag('config', 'G-96HW8XQYYP');
+            `}
+          </Script>
         </Head>
         <body>
           <Main />
